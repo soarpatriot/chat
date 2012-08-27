b8340de9e16869a0cb059680e0807b4bc32b5efe
Soar <85624529@qq.com>
2012-08-28 01:21:47 +0800

    first commit

--- app.js
+++ app.js
@@ -4,11 +4,18 @@
  */
 
 var express = require('express')
+  //, stylus = require('stylus')
+  //, nib = require('nib')
+  , io = require('socket.io')
   , routes = require('./routes')
   , http = require('http')
   , path = require('path');
 
-var app = express();
+
+var app = express()
+    , server = require('http').createServer(app)
+    , io = io.listen(server);
+server.listen(3000);
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -28,6 +35,46 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+
+
+//http.createServer(app).listen(app.get('port'), function(){
+  //console.log("Express server listening on port " + app.get('port'));
+//});
+
+var nicknames = {};
+
+io.sockets.on('connection', function (socket) {
+    socket.on('user message',function(msg){
+        socket.broadcast.emit('user message',socket.nickname, msg);
+        socket.emit('user message', socket.nickname, msg);
+        console.log("nicknames and msg" + socket.nickname+msg );
+
+    });
+
+    socket.on('nickname',function(nick){
+        console.log('nicknames' + nicknames);
+        //if(nicknames[nick]){
+          //  fn(true);
+           // console.log("nicknames 111" + nicknames);
+        //}else{
+          //  fn(false);
+            nicknames[nick] = socket.nickname = nick;
+            socket.broadcast.emit('announcement', nick + ' connected');
+            socket.emit('nickname', nicknames);
+
+            console.log("nicknames " + nicknames);
+
+        //}
+    });
+
+    socket.on('disconnect', function(){
+        if(!socket.nickname){
+            return;
+        }
+        delete  nicknames[socket.nickname];
+        socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
+        socket.broadcast.emit('nicknames', nicknames);
+    });
+
+
 });
