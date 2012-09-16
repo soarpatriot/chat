2f0329d2c4cf736c4d192fe2091740a22784d3c8
Soar <85624529@qq.com>
2012-09-16 23:10:59 +0800

    change chat  to fluid

---
+++ heroku-mongodb-settings.js
@@ -0,0 +1,6 @@
+
+
+To connect using the shell:
+    mongo ds037587.mongolab.com:37587/heroku_app7129634 -u <user> -p <password>
+To connect using a driver via the standard URI (what's this?):
+mongodb://<user>:<password>@ds037587-a.mongolab.com:37587/heroku_app7129634
--- settings.js
+++ settings.js
@@ -4,3 +4,6 @@ module.exports = {
 	host : 'localhost',
 	port : 27017
 }
+
+
+
