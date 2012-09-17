d1a6b8279db99b13fea9e870130d55ec23d9feb7
Soar <85624529@qq.com>
2012-09-17 23:01:58 +0800

    finish two taks

--- routes/index.js
+++ routes/index.js
@@ -47,16 +47,32 @@ exports.post = function(req, res){
 };
 exports.doPost = function(req, res){
     var currentUser = req.session.user;
-    var post = new Post(currentUser.name, req.body.post);
+    var content =  req.body.content;
+
+    if(currentUser === null){
+        req.flash('error','请先登录！ ');
+        return res.redirect('/post');
+    }
+
+    if( content === null ||  content===''){
+        req.flash('error','发言内容不能为！ ');
+        return res.redirect('/post');
+    }
+
+    var post = new Post(currentUser.name, content);
+
+
     post.save(function(err){
         if(err){
             req.flash('error',err);
-            return res.redirect('/');
+            return res.redirect('/post');
         }else{
             req.flash('success','发表成功！');
             res.redirect('/u/'+currentUser.name);
         }
     });
+
+
 }
 
 exports.chat = function(req, res){
