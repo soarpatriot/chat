5abe46df79cf56533656636149f41b7a6c527c0e
Soar <85624529@qq.com>
2013-11-02 20:46:06 +0800

    change home page style add ie8, meta,email send, devices display

---
+++ app/helpers/email.js
@@ -0,0 +1,31 @@
+var nodemailer = require("nodemailer");
+
+// create reusable transport method (opens pool of SMTP connections)
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: "soarpatriot@gmail.com",
+        pass: "22143521"
+    }
+});
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: "Fred Foo ✔ <soarpatriot@126.com>", // sender address
+    to: "85624529@qq.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world ✔", // plaintext body
+    html: "<b>Hello world ✔</b>" // html body
+}
+
+// send mail with defined transport object
+smtpTransport.sendMail(mailOptions, function(error, response){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Message sent: " + response.message);
+    }
+
+    // if you don't want to use this transport object anymore, uncomment following line
+    //smtpTransport.close(); // shut down the connection pool, no more messages
+});
