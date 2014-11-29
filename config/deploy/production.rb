6d4482bef530642262c558c9cf57fb9315261e93
Soar <85624529@qq.com>
2014-11-29 20:36:06 +0800

    change production deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
-set :server_name, "localhost"
+set :server_name, "www.soaror.com"
 
 set :branch, "master"
 set :deploy_to, "/data/www/chat"
 
 
-
+set :node_pid, "#{shared_path}/tmp/pids/chat.pid"
 server fetch(:server_name), user: "soar", roles: %w{web app db}
